7d0fddbdff93a586240d2f3e28ae2cdf9a65dcf8
Soma.Utsumi <soma.utsumi@zuuonline.com>
2018-01-29 19:29:44 +0900

    test

--- read-csv2.go
+++ read-csv2.go
@@ -8,7 +8,33 @@ import (
     "go-test-csv/sample"
 )
 
+
+func Sample() {
+    // テスト用文字列
+    str := "test,テスト\nHello,こんにちは"
+
+    // CSVのReaderを用意
+    r := csv.NewReader(strings.NewReader(str))
+
+    // デリミタ(TSVなら\t, CSVなら,)設定
+    r.Comma = ','
+
+    // 全部読みだす
+    records, err := r.ReadAll()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 各行でループ
+    for _, v := range records {
+        // 1列目
+        // 2列目
+        fmt.Println(v)
+    }
+}
+
+
 func main() {
-  sample.Sample()
+  Sample()
 
 }
